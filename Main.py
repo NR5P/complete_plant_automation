35d744a6ab9b02b9ba44ae0d82c43949b9139473
Nathan Rigg <orangev8zcar@gmail.com>
2018-07-16 13:59:42 -0500

    .

--- Main.py
+++ Main.py
@@ -1,12 +1,20 @@
 
 import RPi.GPIO as GPIO
 from Timer import Timer
+from webApp.app import app
+import threading
 
 def main():
     GPIO.setmode(GPIO.BOARD)
+    threadingStart()
 
-    Timer.run()
 
+def threadingStart():
+    timerThread = threading.Thread(target=Timer.run())
+    appThread = threading.Thread(target=app.run())
+
+    timerThread.start()
+    appThread.start()
 
 if __name__ == "__main__":
     main()
