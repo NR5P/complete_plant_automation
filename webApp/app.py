b6b8c7559e99048b35b4c4ad9c6f843fa69e2ae4
Nathan Rigg <orangev8zcar@gmail.com>
2018-07-16 12:49:09 -0500

    more work on webApp

--- webApp/app.py
+++ webApp/app.py
@@ -4,12 +4,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/main")
 def index():
-    return render_template("home.html")
+    return render_template("main.html")
 
+@app.route("/valves")
 def irrigationValve():
-    return render_template("valve.html")
+    return render_template("valves.html")
 
+@app.route("/lights")
 def lights():
     return render_template("lights.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
